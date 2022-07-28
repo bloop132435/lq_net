5d4d40f7be1c6b40746f8dd86f0dbb480ae3e6bf
Geoffrey Qian <geoffrey.t.qian@gmail.com>
2022-07-28 11:23:18 -0400

    asdf

--- models/layers.py
+++ models/layers.py
@@ -51,6 +51,7 @@ def seq_b_a_c_s(x, conv, relu, bn, skip=None, skip_enable=False):
         #  print(skip.size()) # NOTE out.size() <= skip.size() is always True
         #  print()
         out += torch.nn.functional.interpolate(skip,tuple(list(out.size())[-2:]),mode='bilinear')
+        out += skip.resize(out.size())
     return out
 
 def actv(args=None, negative_slope=0.01, clip_at=None):
