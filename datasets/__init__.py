8fcbc9097813d622ec23925c2765f188397e2547
Geoffrey Qian <geoffrey.t.qian@gmail.com>
2022-07-29 10:57:30 -0400

    asdf

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -1,6 +1,6 @@
 
 from .classification import cifar10_loader, cifar100_loader, imagenet_loader, data_prefetcher
-from .dali import dali_loader
+#  from .dali import dali_loader
 from .fake import fake_loader
 
 def data_loader(name):
@@ -8,7 +8,6 @@ def data_loader(name):
             "cifar10": cifar10_loader,
             "cifar100": cifar100_loader,
             "imagenet": imagenet_loader,
-            "dali": dali_loader,
             "fake": fake_loader,
             "tiny_imagenet": None,
            }[name]
